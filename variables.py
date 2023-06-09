f4ddde7408fa77f42078c44857980a4f728a4d49
Miguel Angel Paredes Perez <miguel.paredesperez1@gmail.com>
2023-06-09 13:22:42 -0600

    02  Variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+#Python es no Tipado
+
+name="Miguel Paredes"
+age=20
+height=1.70
+is_student=True
+children=[]
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
