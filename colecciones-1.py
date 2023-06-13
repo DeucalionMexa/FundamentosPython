83331e20120bc9bfb76921b461d2d38ba7fad15f
Miguel Angel Paredes Perez <miguel.paredesperez1@gmail.com>
2023-06-13 14:57:17 -0600

    08 Listas

---
+++ colecciones-1.py
@@ -0,0 +1,23 @@
+# Listas
+# Entre corchetes []
+my_list = [123,  True, 3.14, "Hi!"]
+print(type(my_list))
+print(my_list)
+
+# Indexing
+print(my_list[0])
+# Slicing
+print(my_list[2:])
+
+students = ["Lalokota", "Paquiño", "JersitaBB"]
+print(students)
+students.append("Migue")
+students.append("Kike")
+students.append("Gaby")
+print(students)
+students.remove("Lalokota")
+print(students)
+students.remove("Lalokota")
+print(students)
+students[1] = "Zuly"
+print(students)
