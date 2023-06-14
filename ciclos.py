cd33242f7ae3ffd6b7ec6c64225a2a9be7906370
Miguel Angel Paredes Perez <miguel.paredesperez1@gmail.com>
2023-06-14 11:56:25 -0600

    11 Ciclos

---
+++ ciclos.py
@@ -0,0 +1,26 @@
+ # while
+ # while exp_booleana
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
+
+# for -> iterable
+my_str  = "Hola"
+for letter in my_str:
+    print(letter, end =' ')
+print()
+my_lista = ["a", "b", "c", 12]
+for item in my_lista:
+    print(item)
+
+# function range()
+# range(fin)
+for i in range(3):
+    print(i)
+# range(ini:fin)
+for i in range(3, 6):
+    print(i)
+# range(ini:fin:step)
+for i in range(1, 11, 2):
+    print(i, end = ' ')
